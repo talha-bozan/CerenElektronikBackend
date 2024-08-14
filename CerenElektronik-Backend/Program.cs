e6fc595a98e49a1932ccc9bb04aa8f9a1951bbb2
talhabozan <talhabozan@gmail.com>
2024-08-14 05:37:05 +0300

    Allow cors policy

--- CerenElektronik-Backend/Program.cs
+++ CerenElektronik-Backend/Program.cs
@@ -1,13 +1,33 @@
 using CerenElektronik_Backend.Data;
+using static Microsoft.AspNetCore.Http.StatusCodes;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Configure Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins("http://127.0.0.1:5500")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod());
+});
+
+// Add Https Redirection
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.RedirectStatusCode = Status307TemporaryRedirect;
+    options.HttpsPort = 5001;
+});
+
+// Register the QuotationStore service
 builder.Services.AddSingleton<QuotationStore>();
 
 var app = builder.Build();
@@ -18,9 +38,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseStaticFiles();
 
 app.UseHttpsRedirection();
+
+// Use CORS
+app.UseCors("AllowSpecificOrigin");
 
 app.UseAuthorization();
 
